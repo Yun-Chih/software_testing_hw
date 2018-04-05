9fea88b509fdc6078d7208060d72503ff77e87f7
Yun-Chih <j4bdl8suh@gmail.com>
2018-04-05 05:17:51 -0700

    add strong robust testcases which are also weak robust testcases. existing feature code pass the tests.

--- triangle_test.cpp
+++ triangle_test.cpp
@@ -20,6 +20,19 @@ TEST(TriangleTest, WeakRobust) {
 }
 
 TEST(TriangleTest, StrongRobust) {
+  EXPECT_STREQ("Isosceles", Triangle(2, 3, 2));
+  EXPECT_STREQ("Isosceles", Triangle(3, 2, 2));
+  EXPECT_STREQ("Scalene", Triangle(3, 5, 4));
+  EXPECT_STREQ("Scalene", Triangle(4, 5, 3));
+  EXPECT_STREQ("Scalene", Triangle(4, 3, 5));
+  EXPECT_STREQ("Scalene", Triangle(5, 4, 3));
+  EXPECT_STREQ("Scalene", Triangle(5, 3, 4));
+  EXPECT_STREQ("Not a triangle", Triangle(4, 2, 1));
+  EXPECT_STREQ("Not a triangle", Triangle(1, 4, 2));
+  EXPECT_STREQ("Not a triangle", Triangle(1, 2, 4));
+  EXPECT_STREQ("Not a triangle", Triangle(2, 1, 4));
+  EXPECT_STREQ("Not a triangle", Triangle(2, 4, 1));
+
   EXPECT_STREQ("Not a triangle", Triangle(-1, -1, 5));
   EXPECT_STREQ("Not a triangle", Triangle(-1, 201, 5));
   EXPECT_STREQ("Not a triangle", Triangle(201, -1, 5));
