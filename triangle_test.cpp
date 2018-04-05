f1044e9d725dc0646f1053ce5ecf73aa3ce27b59
Yun-Chih <j4bdl8suh@gmail.com>
2018-04-05 04:51:23 -0700

    add weak robust and strong robust testcase. pass the former but fail the latter.

--- triangle_test.cpp
+++ triangle_test.cpp
@@ -9,6 +9,39 @@ TEST(TriangleTest, WeakNormal) {
   EXPECT_STREQ("Scalene", Triangle(3, 4, 5));
   EXPECT_STREQ("Not a triangle", Triangle(4, 1, 2));
 }
+
+TEST(TriangleTest, WeakRobust) {
+  EXPECT_STREQ("Not a triangle", Triangle(-1, 5, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(5, -1, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(5, 5, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(201, 5, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(5, 201, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(5, 5, 201));
+}
+
+TEST(TriangleTest, StrongRobust) {
+  EXPECT_STREQ("Not a triangle", Triangle(-1, -1, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(-1, 201, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(201, -1, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(201, 201, 5));
+  EXPECT_STREQ("Not a triangle", Triangle(-1, 5, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(-1, 5, 201));
+  EXPECT_STREQ("Not a triangle", Triangle(201, 5, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(201, 5, 201));
+  EXPECT_STREQ("Not a triangle", Triangle(5, -1, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(5, -1, 201));
+  EXPECT_STREQ("Not a triangle", Triangle(5, 201, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(5, 201, 201));
+
+  EXPECT_STREQ("Not a triangle", Triangle(-1, -1, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(-1, -1, 201));
+  EXPECT_STREQ("Not a triangle", Triangle(-1, 201, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(-1, 201, 201));
+  EXPECT_STREQ("Not a triangle", Triangle(201, -1, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(201, -1, 201));
+  EXPECT_STREQ("Not a triangle", Triangle(201, 201, -1));
+  EXPECT_STREQ("Not a triangle", Triangle(201, 201, 201));
+}
 }
 
 
