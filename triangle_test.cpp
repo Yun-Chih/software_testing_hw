f32750ee57f37760c9df25bc01d31da6d92dfebb
Yun-Chih <j4bdl8suh@gmail.com>
2018-04-05 03:56:40 -0700

    start Triangle problem. after equivalence class analyses, add weak normal testcase.

---
+++ triangle_test.cpp
@@ -0,0 +1,14 @@
+#include "triangle.h"
+#include "gtest/gtest.h"
+#include "gtest/gtest.h"
+
+namespace {
+TEST(TriangleTest, WeakNormal) {
+  EXPECT_STREQ("Equilateral", Triangle(5, 5, 5));
+  EXPECT_STREQ("Isosceles", Triangle(2, 2, 3));
+  EXPECT_STREQ("Scalene", Triangle(3, 4, 5));
+  EXPECT_STREQ("Not a trangle", Triangle(4, 1, 2));
+}
+}
+
+
